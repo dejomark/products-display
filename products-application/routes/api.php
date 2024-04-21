21fa002273f808b551fd2e4483f1a6820f9f3c03
dejomark <dejanmarkovic645@gmail.com>
2024-04-21 20:11:04 +0200

    feat: implemented getAllProducts endpoint [getAllProducts fetches all of the products in the database]

--- products-application/routes/api.php
+++ products-application/routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/getAllProducts', [ProductController::class, 'index']);

26bfb25c2382dcd8e5c1b0ba5466b926a86188d0
dejomark <dejanmarkovic645@gmail.com>
2024-04-22 11:38:46 +0200

    feat: impl getAllComments endpoint [getAllComments fetches all comments]

--- products-application/routes/api.php
+++ products-application/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\api\CommentController;
 use App\Http\Controllers\api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -9,3 +10,5 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 Route::get('/getAllProducts', [ProductController::class, 'index']);
+
+Route::get('/getAllComments', [CommentController::class, 'index']);

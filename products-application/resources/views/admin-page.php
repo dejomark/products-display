e3293cf9f7ee7bca2e4bcfefb78cea15e99f2fa6
dejomark <dejanmarkovic645@gmail.com>
2024-04-24 19:12:40 +0200

    feat: added protected admin page

---
+++ products-application/resources/views/admin-page.php
@@ -0,0 +1,6 @@
+<?php
+    session_start(); 
+    if (isset($_SESSION['sessionId']) && isset($_SESSION['admin'])) {
+        require 'comment-verify.php';
+    }
+?>
---
+++ products-application/resources/views/comment-verify.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Admin page </title>
+    </head>
+    <body>
+        Admin page <br>
+        <form action="api/auth/logout" method="post">
+            <input type="submit" value="Log out">
+        </form>
+    </body>
+</html>

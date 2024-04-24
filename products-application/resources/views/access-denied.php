752f0224d5f6dc1117dcd917f16e93f57283b648
dejomark <dejanmarkovic645@gmail.com>
2024-04-24 19:27:56 +0200

    feat: added acces denied page [it displays when you go to admin page directly without logging in]

---
+++ products-application/resources/views/access-denied.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Access denied </title>
+    </head>
+    <body>
+        Nice try, bub. Now, be a good user and 
+        <a href="/">Return to main page</a>
+    </body>
+</html>
--- products-application/resources/views/admin-page.php
+++ products-application/resources/views/admin-page.php
@@ -2,5 +2,8 @@
     session_start(); 
     if (isset($_SESSION['sessionId']) && isset($_SESSION['admin'])) {
         require 'comment-verify.php';
+    } else {
+        require 'access-denied.php';
     }
+    
 ?>

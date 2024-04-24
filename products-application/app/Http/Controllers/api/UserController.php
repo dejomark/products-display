9703ace9825fac40baf86e58d3350cae38b3144a
dejomark <dejanmarkovic645@gmail.com>
2024-04-24 19:08:47 +0200

    feat: made controller for users to implement authorization [all users are admins as things stand]

---
+++ products-application/app/Http/Controllers/api/UserController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function loginUser(Request $request)
+    {
+        try {
+            $validateUser = Validator::make($request->all(), 
+            [
+                'email' => 'required|email',
+                'password' => 'required'
+            ]);
+
+            if($validateUser->fails()){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'validation error',
+                    'errors' => $validateUser->errors()
+                ], 401);
+            }
+
+            if(!Auth::attempt($request->only(['email', 'password']))){
+                return response()->json([
+                    'status' => false,
+                    'message' => 'Email & Password does not match with our record.',
+                ], 401);
+            }
+
+            $user = User::where('email', $request->email)->first();
+
+            session_start();
+            session_destroy();
+
+            session_start();
+            if (!isset($_SESSION['sessionId'])) {
+                session_regenerate_id(); 
+                $_SESSION['sessionId'] = $user->id;
+            }
+            $_SESSION['admin'] = $user->name;
+
+            return redirect('/admin');
+
+        } catch (\Throwable $th) {
+            return response()->json([
+                'status' => false,
+                'message' => $th->getMessage()
+            ], 500);
+        }
+    }
+
+    public function logoutUser() {
+        session_start();
+        session_destroy();
+
+        return redirect('/');
+    }
+}
--- products-application/routes/api.php
+++ products-application/routes/api.php
@@ -2,12 +2,17 @@
 
 use App\Http\Controllers\api\CommentController;
 use App\Http\Controllers\api\ProductController;
+use App\Http\Controllers\api\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::post('/auth/login', [UserController::class, 'loginUser']);
+
+Route::post('/auth/logout', [UserController::class, 'logoutUser']);
 
 Route::get('/getAllProducts', [ProductController::class, 'index']);
 
